a5d452f410108771d5b8a539a2a1ad9a27490938
Reese <Reese2.Killick@live.uwe.ac.uk>
2025-10-19 16:34:10 +0100

    state changes
    with switch statements

--- Assets/Scripts/Game manager.cs
+++ Assets/Scripts/Game manager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
 
     public enum GameState
@@ -11,7 +11,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     public GameState state;
-
+    public bool has_changed_state = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,22 +22,51 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Checks the game state we are in
-        if (state == GameState.GAMEPLAY)
+        switch (state)
         {
-            //Toggle state over the return key
-            if(Input.GetKeyDown(KeyCode.Escape))
-            {
-                state = GameState.PAUSE;
-            }
-        }
-        else if (state == GameState.PAUSE)
-        {
-            // Toggle state over the return key
-            if(Input.GetKeyDown(KeyCode.Escape))
-            {
-                state = GameState.GAMEPLAY;
-            }
+            // Checks the game state we are in
+            case GameState.GAMEPLAY:
+                // Toggle pause over the return key
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    state = GameState.PAUSE;
+                    has_changed_state = true;
+                }
+                break;
+            case GameState.PAUSE:
+                // Toggle play over the return key
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    state = GameState.GAMEPLAY;
+                    has_changed_state = true;
+                }
+                break;
+            default:
+                break;
         }
     }
+
+    private void LateUpdate()
+    {
+        // checking if the game has been paused
+        if (has_changed_state == true)
+        {
+            switch (state)
+            {
+                case GameState.PAUSE:
+                    Time.timeScale = 0.0f;
+                    break;
+                case GameState.GAMEPLAY:
+                    Time.timeScale = 1.0f;
+                    break;
+                default:
+                    Time.timeScale = 1.0f;
+                    break;
+            }
+
+            has_changed_state = false;
+        }
+    }
+
+
 }
